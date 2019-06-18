7b06ab497c44f133265566511d22c5f2c7248d08
Алексей <rylovalex2108l@mail.ru>
2019-06-18 21:19:58 +0700

    доработал главную страницу

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function findElement(elem, parent, cls) {
+  while (elem !== parent) {
+    if (elem.classList.contains(cls)) {
+      return elem;
+    }
+    elem = elem.parentNode;
+  }
+  return false;
+}
+
+document.addEventListener('click', function (event) {
+  var target = event.target;
+  var currentTarget = event.currentTarget;
+  var jsOpenElement = findElement(target, currentTarget, 'js-open');
+
+  if (jsOpenElement) {
+    event.preventDefault();
+    var modalWrapperElement = document.querySelector('.js-' + jsOpenElement.dataset.name);
+    modalWrapperElement.classList.remove('hidden');
+    modalWrapperElement.classList.add('overlay');
+  }
+});
+
+document.addEventListener('click', function (event) {
+  var target = event.target;
+  var currentTarget = event.currentTarget;
+
+  if (target.classList.contains('js-remove')) {
+    var parentElement = findElement(target, currentTarget, 'modal-wrapper')
+    parentElement.classList.remove('overlay');
+    parentElement.classList.add('hidden');
+  }
+});
