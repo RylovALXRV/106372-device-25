8828f7056d8a73a07ee84dee1e1b05a0ceefd7c3
Алексей <rylovalex2108l@mail.ru>
2019-06-23 20:41:34 +0700

    написал стили для каталога, изменил разметку на главной странице для слайдеров, унифицировал js

--- js/script.js
+++ js/script.js
@@ -10,26 +10,66 @@ function findElement(elem, parent, cls) {
   return false;
 }
 
+function changeSlider (elem, parent) {
+  var dataSlider = elem.dataset.slider;
+  var sliderItemElements = document.querySelectorAll('.' + parent.id + '__item');
+
+  for (var i = 0; i < sliderItemElements.length; i++) {
+    var sliderItemElement = sliderItemElements[i];
+
+    if (sliderItemElement.id === dataSlider) {
+      sliderItemElement.classList.remove('hidden');
+      continue;
+    }
+
+    sliderItemElement.classList.add('hidden');
+  }
+}
+
+function changeActiveButton (elem, parent) {
+  if (elem.classList.contains(parent.id + '__btn--active')) {
+    return;
+  }
+
+  var sliderBtnElements = document.querySelectorAll('.' + parent.id + '__btn');
+
+  for (var i = 0; i < sliderBtnElements.length; i++) {
+    var sliderBtnElement = sliderBtnElements[i];
+
+    sliderBtnElement.classList.remove(parent.id + '__btn--active');
+  }
+
+  elem.classList.add(parent.id + '__btn--active');
+}
+
 document.addEventListener('click', function (event) {
   var target = event.target;
   var currentTarget = event.currentTarget;
-  var jsOpenElement = findElement(target, currentTarget, 'js-open');
+  var parentElement = findElement(target, currentTarget, 'js-open');
 
-  if (jsOpenElement) {
+  if (parentElement && parentElement.classList.contains('js-open')) {
     event.preventDefault();
-    var modalWrapperElement = document.querySelector('.js-' + jsOpenElement.dataset.name);
+
+    var modalWrapperElement = document.querySelector('.js-' + parentElement.dataset.name);
+
     modalWrapperElement.classList.remove('hidden');
     modalWrapperElement.classList.add('overlay');
   }
-});
-
-document.addEventListener('click', function (event) {
-  var target = event.target;
-  var currentTarget = event.currentTarget;
 
   if (target.classList.contains('js-remove')) {
-    var parentElement = findElement(target, currentTarget, 'modal-wrapper')
+    event.preventDefault();
+
+    parentElement = findElement(target, currentTarget, 'modal-wrapper');
+
     parentElement.classList.remove('overlay');
     parentElement.classList.add('hidden');
+  }
+
+  if (target.tagName === 'BUTTON' && target.dataset.slider) {
+
+    parentElement = findElement(target, currentTarget, 'js-slider')
+
+    changeActiveButton(target, parentElement);
+    changeSlider(target, parentElement);
   }
 });
